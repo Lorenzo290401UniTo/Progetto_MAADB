74ec369de570267e7fcd1ddf3ae84f0410820bdb
Lorenzo Mori <lorenzo.mori31@edu.unito.it>
2025-06-23 17:33:44 +0200

    Commit #7
    - Fix & error checks on third parametric query

--- src/app/api/parametriche_query3/route.tsx
+++ src/app/api/parametriche_query3/route.tsx
@@ -2,13 +2,21 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/app/api/mongo_init";
 import driver from "@/app/api/neo_init";
 
-export async function GET() {
+export async function GET(req: Request) {
   const session = driver.session();
+
+  const url = new URL(req.url);
+  let personId = 0;
+  let param = url.searchParams.get("id");
+  if (param != null && param != "") {
+    personId = parseInt(param);
+  } else {
+    return NextResponse.json({error: "User ID non inserito"})
+  }
 
   try {
     const client = await clientPromise;
     const db = client.db("Progetto_MAADB");
-    let personId = 8333;
 
     // Seleziona la città di una certa persona
     const result = await session.run(`MATCH (p:person {id: $personId})-[:islocatedin]->(pl:place) WHERE pl.type = "city" RETURN pl.name AS city`, { personId });
@@ -16,16 +24,22 @@ export async function GET() {
     //console.log(city);
 
     // Seleziona tutti gli abitanti di quella città
-    const people = await session.run(`MATCH (p:person)-[:islocatedin]->(pl:place {type: "city", name: $city}) RETURN p.id AS person_id`, { city });
-    let people_list = people.records.map((record) => record.get("person_id").toNumber());
-    people_list = people_list.filter((item) => item != personId);
+    let people_list
+    if(city != undefined){
+      const people = await session.run(`MATCH (p:person)-[:islocatedin]->(pl:place {type: "city", name: $city}) RETURN p.id AS person_id`, { city });
+      people_list = people.records.map((record) => record.get("person_id").toNumber());
+      people_list = people_list.filter((item) => item != personId);
+    }else{
+      return NextResponse.json({error: "Nessuna città associata all'utente " + personId})
+    }
     //console.log(people_list);
 
     // Seleziona i conoscenti della persona in input
     const users = await db.collection("person_knows_person").find({ Person_id: personId }).toArray();
     let knownPeople = users.map((record) => record.Person_id_1);
-    //console.log(studentsList)
+    //console.log(knownPeople);
 
+    // Incrocio i conoscenti dell'utente con quelli della sua stessa città
     let finalList = [] as number[];
     people_list.forEach((element) => {
       if (knownPeople.includes(element)) {
@@ -34,11 +48,43 @@ export async function GET() {
     });
     //console.log(finalList);
 
+    // Seleziona i forum dei conoscenti della persona in input
     const forums = await db
       .collection("forum_hasMember_person")
-      .find({ Person_id: { $in: finalList } })
+      .aggregate([
+        { $match: { Person_id: { $in: finalList } } },
+        {
+          $group: {
+            _id: "$Person_id",
+            forums: { $push: "$$ROOT" },
+          },
+        },
+      ])
       .toArray();
-    let res = forums.map((record) => [record.Person_id, record.Forum_id]);
+
+    // Conversione id => dati
+    let res = [] as any;
+    for (let element of forums) {
+      let person_res = await db
+        .collection("person")
+        .find({ id: (element as unknown as any)._id }, { projection: { _id: 0, firstName: 1, lastName: 1 } })
+        .toArray();
+      let person_name = person_res.map((record) => record.firstName + " " + record.lastName)[0];
+
+      let forums = (element as unknown as any).forums.map((record: any) => record.Forum_id);
+
+      let f_titles = await db
+        .collection("forum")
+        .find({ id: { $in: forums } }, { projection: { _id: 0, title: 1 } })
+        .toArray();
+      f_titles = f_titles.map((record) => record.title);
+
+      res.push({
+        user: person_name,
+        forums: f_titles,
+      });
+    }
+    //console.log(res);
 
     return NextResponse.json(res);
   } catch (error) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image"
 import { useState } from "react";
 import axios from "axios";
 import "@/app/page.css";
@@ -13,10 +13,15 @@ type resType = {
 };
 
 export default function mainPage() {
+  // gestione UI
+  const [selectedQuery, setSelectedQuery] = useState(0)
+
   // risultati query analitica 1
   const [europeanStudents, setEuropeanStudents] = useState(0);
   const [totalStudents, setTotalStudents] = useState(0);
   const [europeanPercentage, setEuropeanPercentage] = useState(0);
+  const [showResultsQuery1, setShowResultsQuery1] = useState(false)
+  const [isLoadingQuery1, setIsLoadingQuery1] = useState(false)
 
   // risultati query analitica 3
   const [analitica3Results, setAnalitica3Results] = useState([] as string[]);
@@ -28,19 +33,26 @@ export default function mainPage() {
   const [parametrica2Results, setParametrica2Results] = useState<resType[]>([]);
 
   // risultati query parametrica 3
-  const [parametrica3Results, setParametrica3Results] = useState<[number, number][]>([]);
+  const [parametrica3Results, setParametrica3Results] = useState<[string, string[]][]>([]);
+  const [userIDInput, setUserIDInput] = useState("")
+  const [query3Error, setQuery3Error] = useState("")
 
   /* ----------- CHIAMATE API (QUERY ANALITICHE) ----------- */
 
   async function analitiche_query1() {
     try {
+      setIsLoadingQuery1(true)
+      setShowResultsQuery1(false)
       const res = await axios.get("/api/analitiche_query1");
 
       setEuropeanStudents(res.data[0]["low"]);
       setTotalStudents(res.data[1]["low"]);
       setEuropeanPercentage(res.data[2]);
+      setShowResultsQuery1(true)
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoadingQuery1(false)
     }
   }
 
@@ -114,11 +126,23 @@ export default function mainPage() {
 
   async function parametriche_query3() {
     try {
-      // manca input ID persona
-      const res = await axios.get("/api/parametriche_query3");
-      res.data.forEach((record: any) => {
-        setParametrica3Results((prev) => [...prev, [record[0], record[1]]]);
+      setQuery3Error("")
+      const res = await axios.get("/api/parametriche_query3", {
+        params: {
+          id: userIDInput
+        }
       });
+      if(res.data.error == undefined){
+        if(res.data.length != 0){
+          res.data.forEach((record: any) => {
+            setParametrica3Results((prev) => [...prev, [record.user, record.forums]]);
+          });
+        }else{
+          setQuery3Error("Nessun dato trovato")
+        }
+      }else{
+        setQuery3Error(res.data.error)
+      }
     } catch (error) {
       console.error(error);
     }
@@ -126,90 +150,116 @@ export default function mainPage() {
 
   return (
     <>
-      <main className="mx-[15%]">
-        <h1 className="text-[40px]" style={{ fontWeight: "bold" }}>
+      <main className="text-[white] text-[18px]">   {/* mr-[15%] */}
+        <h1 className="text-[42px]" style={{ fontWeight: "bold" }}>
           Progetto MAADB
         </h1>
-
-        <div className="border-[2px] border-solid border-[red] px-[20px]">
-          <p>Percentuale di studenti europei sul totale degli studenti nel mondo</p>
-          <button onClick={() => analitiche_query1()}>Query 1</button>
-          <div>
-            <p>
-              <span style={{ fontWeight: "bold" }}>Studenti europei</span>: {europeanStudents}
-            </p>
-            <p>
-              <span style={{ fontWeight: "bold" }}>Studenti totali</span>: {totalStudents}
-            </p>
-            <p>
-              <span style={{ fontWeight: "bold" }}>Percentuale di europei sul totale</span>: {europeanPercentage}%
-            </p>
+        <div className="w-full h-[80vh] flex">
+          <div className="w-[15%] h-[100vdh] flex flex-col gap-[5px] pr-[25px]" style={{borderRight: "1px solid white"}}>
+            <p className="text-[20px]">Query analitiche:</p>
+            <button className="w-full h-[50px] bg-[#FFFFC5] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(0)}>Query 1</button>
+            <button className="w-full h-[50px] bg-[#FF7F7F] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(1)}>Query 2</button>
+            <button className="w-full h-[50px] bg-[#FFFFC5] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(2)}>Query 3</button>
+            <p className="text-[20px]">Query parametriche:</p>
+            <button className="w-full h-[50px] bg-[#FFFFC5] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(3)}>Query 1</button>
+            <button className="w-full h-[50px] bg-[#FFFFC5] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(4)}>Query 2</button>
+            <button className="w-full h-[50px] text-[16px] rounded-[6px] cursor-pointer" onClick={() => setSelectedQuery(5)}>Query 3</button>
           </div>
-        </div>
+          <div className="w-[85%] ml-[25px]">
+            <div className="flex flex-col" style={selectedQuery != 0 ? {display: "none"} : {}}>
+              <p className="text-[20px]">Percentuale di studenti europei sul totale degli studenti nel mondo</p>
+              <button className="w-[150px] h-[50px] px-[20px] text-[16px] rounded-[6px] cursor-pointer" onClick={() => analitiche_query1()}>Esegui query</button>
+              <Image src="/loading_icon.gif" style={!isLoadingQuery1 ? {display: "none"} : {}} width={250} height={150} alt="loading_icon" priority/>
+              <div style={!showResultsQuery1 ? {display : "none"} : {}}>
+                <p>
+                  <span style={{ fontWeight: "bold" }}>Studenti europei</span>: {europeanStudents}
+                </p>
+                <p>
+                  <span style={{ fontWeight: "bold" }}>Studenti totali</span>: {totalStudents}
+                </p>
+                <p>
+                  <span style={{ fontWeight: "bold" }}>Percentuale di europei sul totale</span>: {europeanPercentage}%
+                </p>
+              </div>
+            </div>
 
-        <div className="border-[2px] border-solid border-[red] px-[20px] pb-[20px]">
-          <p>Seleziona i tag dei top 10 forum</p>
-          <button onClick={() => analitiche_query2()}>Query 2</button>
-        </div>
+            <div className="pb-[20px]" style={selectedQuery != 1 ? {display: "none"} : {}}>
+              <p>Seleziona i tag dei top 10 forum</p>
+              <button onClick={() => analitiche_query2()}>Query 2</button>
+            </div>
 
-        <div className="border-[2px] border-solid border-[red] px-[20px] pb-[20px]">
-          <p>Contare il numero di studenti lavoratori per città e per sesso</p>
-          <button onClick={() => analitiche_query3()}>Query 3</button>
-          <div>
-            {analitica3Results.map((element, index) => (
-              <p key={index}>
-                <span style={{ fontWeight: "bold" }}>{element.split("#")[0]}</span>
-                <br />
-                Males: {element.split("#")[1]}
-                <br />
-                Females: {element.split("#")[2]}
-              </p>
-            ))}
-          </div>
-        </div>
-
-        <div className="border-[2px] border-solid border-[blue] px-[20px] pb-[20px] mt-[50px]">
-          <p>Selezionare tutti i messaggi creati da un determinato (genere group by ID)</p>
-          <button onClick={() => parametriche_query1()}>Query 1</button>
-          <div>
-            {parametrica1Results.map((record, index) => (
-              <p key={index}>
-                {record[0]}
-                <br />
-                {record[1]}
-              </p>
-            ))}
-          </div>
-        </div>
-
-        <div className="border-[2px] border-solid border-[blue] px-[20px] pb-[20px]">
-          <p>Selezionare tutti i commenti ai messaggi scritti da persone con un certo interesse</p>
-          <button onClick={() => parametriche_query2()}>Query 2</button>
-          <div>
-            {parametrica2Results.map((element, index) => (
-              <p key={index} style={{ fontWeight: "bold" }}>
-                {element.name}
-                <br />
-                {element.msgs.map((element, index) => (
-                  <span key={index} style={{ fontWeight: "normal" }}>
-                    - {element.id}: {element.content}
+            <div className="pb-[20px]" style={selectedQuery != 2 ? {display: "none"} : {}}>
+              <p>Contare il numero di studenti lavoratori per città e per sesso</p>
+              <button onClick={() => analitiche_query3()}>Query 3</button>
+              <div>
+                {analitica3Results.map((element, index) => (
+                  <p key={index}>
+                    <span style={{ fontWeight: "bold" }}>{element.split("#")[0]}</span>
                     <br />
-                  </span>
+                    Males: {element.split("#")[1]}
+                    <br />
+                    Females: {element.split("#")[2]}
+                  </p>
                 ))}
-              </p>
-            ))}
-          </div>
-        </div>
+              </div>
+            </div>
 
-        <div className="border-[2px] border-solid border-[blue] px-[20px] pb-[20px]">
-          <p>Considerando una persona con un determinato ID, restituire i forum di cui sono membri i conoscenti di tale persona localizzati nella sua stessa città</p>
-          <button onClick={() => parametriche_query3()}>Query 3</button>
-          <div>
-            {parametrica3Results.map((record, index) => (
-              <p key={index}>
-                Utente {record[0]} partecipa al forum {record[1]}
-              </p>
-            ))}
+            <div className="pb-[20px" style={selectedQuery != 3 ? {display: "none"} : {}}>
+              <p>Selezionare tutti i messaggi creati da un determinato (genere group by ID)</p>
+              <button onClick={() => parametriche_query1()}>Query 1</button>
+              <div>
+                {parametrica1Results.map((record, index) => (
+                  <p key={index}>
+                    {record[0]}
+                    <br />
+                    {record[1]}
+                  </p>
+                ))}
+              </div>
+            </div>
+
+            <div className="max-h-[100%] flex flex-col pb-[20px] overflow-hidden" style={selectedQuery != 4 ? {display: "none"} : {}}>
+              <p>Selezionare tutti i commenti ai messaggi scritti da persone con un certo interesse</p>
+              <button onClick={() => parametriche_query2()}>Query 2</button>
+              <div className="overflow-y-scroll">
+                {parametrica2Results.map((element, index) => (
+                  <p key={index} style={{ fontWeight: "bold" }}>
+                    {element.name}
+                    <br />
+                    {element.msgs.map((element, index) => (
+                      <span key={index} style={{ fontWeight: "normal" }}>
+                        - {element.id}: {element.content}
+                        <br />
+                      </span>
+                    ))}
+                  </p>
+                ))}
+              </div>
+            </div>
+
+            <div className="pb-[20px] flex flex-col gap-[10px]" style={selectedQuery != 5 ? {display: "none"} : {}}>
+              <p>Considerando una persona con un determinato ID, restituire i forum di cui sono membri i conoscenti di tale persona localizzati nella sua stessa città</p>
+              <div className="flex gap-[15px] items-center">
+                <label>Person ID: </label>
+                <input type="text" className="h-[30px] rounded-[6px] outline-none text-[18px]" value={userIDInput} onChange={(event) => setUserIDInput(event.target.value)}></input>
+                <button className="w-[150px] h-[50px] px-[20px] text-[16px] rounded-[6px] cursor-pointer" onClick={() => parametriche_query3()}>Esegui query</button>
+              </div>
+              <div>
+                {
+                  query3Error == "" ?
+                    parametrica3Results.map((record, index) => (
+                      <p key={index}>
+                        <span style={{ fontWeight: "bold" }}>{record[0]}</span><br/>
+                        {
+                          record[1].map((element, index) => {
+                            return <span key={index}> - {element}<br/></span>
+                          })
+                        }
+                      </p>
+                    )) : query3Error
+                }
+              </div>
+            </div>
           </div>
         </div>
       </main>
